f15d798260fcc5e26c0d423fbfb5c9c9b845b4b1
Anusree Purushothaman <anusreepurushothaman@MacBook-Pro.local>
2025-09-23 11:33:07 +0530

    changes

--- src/components/Startups.svelte
+++ src/components/Startups.svelte
@@ -1,64 +1,398 @@
 <script>
   // Example static JSON data (replace with API fetch if needed)
-  let data = [
+ let data = [
+   {
+     id: 1,
+     attributes: {
+       Type: "Healthcare",
+       Startups: {
+         data: [
+           {
+             id: 1,
+             attributes: {
+               Label: "Startup A",
+               order: 1,
+               Image: {
+                 data: {
+                   attributes: { url: "/img/startups/1s.png" }
+                 }
+               }
+             }
+           },
+           {
+             id: 2,
+             attributes: {
+               Label: "Startup B",
+               Websitelink: "",
+               order: 2,
+               Image: {
+                 data: {
+                   attributes: { url: "/img/startups/2s.png" }
+                 }
+               }
+             }
+           },
+           {
+             id: 3,
+             attributes: {
+               Label: "Startup B",
+               Websitelink: "",
+               order: 2,
+               Image: {
+                 data: {
+                   attributes: { url: "/img/startups/3s.png" }
+                 }
+               }
+             }
+           },
+           {
+             id: 4,
+             attributes: {
+               Label: "Startup B",
+               Websitelink: "",
+               order: 2,
+               Image: {
+                 data: {
+                   attributes: { url: "/img/startups/4s.png" }
+                 }
+               }
+             }
+           },
+           {
+             id: 5,
+             attributes: {
+               Label: "Startup B",
+               Websitelink: "",
+               order: 2,
+               Image: {
+                 data: {
+                   attributes: { url: "/img/startups/5s.png" }
+                 }
+               }
+             }
+           },
+           {
+             id: 6,
+             attributes: {
+               Label: "Startup B",
+               Websitelink: "",
+               order: 2,
+               Image: {
+                 data: {
+                   attributes: { url: "/img/startups/6s.png" }
+                 }
+               }
+             }
+           },
+           {
+             id: 7,
+             attributes: {
+               Label: "Startup B",
+               Websitelink: "",
+               order: 2,
+               Image: {
+                 data: {
+                   attributes: { url: "/img/startups/7s.png" }
+                 }
+               }
+             }
+           },
+           {
+             id: 8,
+             attributes: {
+               Label: "Startup B",
+               Websitelink: "",
+               order: 2,
+               Image: {
+                 data: {
+                   attributes: { url: "/img/startups/8s.png" }
+                 }
+               }
+             }
+           },
+           {
+             id: 9,
+             attributes: {
+               Label: "Startup B",
+               Websitelink: "",
+               order: 2,
+               Image: {
+                 data: {
+                   attributes: { url: "/img/startups/9s.png" }
+                 }
+               }
+             }
+           }
+         ]
+       }
+     }
+   },
+   {
+     id: 2,
+     attributes: {
+       Type: "IT/ITeS",
+       Startups: {
+         data: [
+           {
+             id: 10,
+             attributes: {
+               Label: "Startup C",
+               order: 1,
+               Image: {
+                 data: {
+                   attributes: { url: "/img/Startups/10s.png" }
+                 }
+               }
+             }
+           },
+           {
+             id: 11,
+             attributes: {
+               Label: "Startup C",
+               order: 1,
+               Image: {
+                 data: {
+                   attributes: { url: "/img/Startups/11s.png" }
+                 }
+               }
+             }
+           },
+           {
+             id: 12,
+             attributes: {
+               Label: "Startup C",
+               order: 1,
+               Image: {
+                 data: {
+                   attributes: { url: "/img/Startups/12s.png" }
+                 }
+               }
+             }
+           },
+           {
+             id: 40,
+             attributes: {
+               Label: "QuickDine",
+               order: 1,
+               Image: {
+                 data: {
+                   attributes: { url: "/img/Startups/40s.png" }
+                 }
+               }
+             }
+           }
+         ]
+       }
+     }
+   },
     {
-      id: 1,
-      attributes: {
-        Type: "Healthcare",
-        Startups: {
-          data: [
-            {
-              id: 101,
-              attributes: {
-                Label: "Startup A",
-                order: 1,
-                Image: {
-                  data: {
-                    attributes: { url: "/img/Startups/Startup-a.png" }
-                  }
-                }
-              }
-            },
-            {
-              id: 102,
-              attributes: {
-                Label: "Startup B",
-                Websitelink: "",
-                order: 2,
-                Image: {
-                  data: {
-                    attributes: { url: "/img/Startups/Startup-b.png" }
-                  }
-                }
-              }
-            }
-          ]
-        }
-      }
-    },
+     id: 3,
+     attributes: {
+       Type: "Agriculture & Lifesciences",
+       Startups: {
+         data: [
+           {
+             id: 13,
+             attributes: {
+               Label: "Startup C",
+               order: 1,
+               Image: {
+                 data: {
+                   attributes: { url: "/img/Startups/13s.png" }
+                 }
+               }
+             }
+           },
+           {
+             id: 14,
+             attributes: {
+               Label: "Startup C",
+               order: 1,
+               Image: {
+                 data: {
+                   attributes: { url: "/img/Startups/14s.png" }
+                 }
+               }
+             }
+           },
+           {
+             id: 15,
+             attributes: {
+               Label: "Startup C",
+               order: 1,
+               Image: {
+                 data: {
+                   attributes: { url: "/img/Startups/15s.png" }
+                 }
+               }
+             }
+           },
+         
+         ]
+       }
+     }
+   },
     {
-      id: 2,
-      attributes: {
-        Type: "IT/ITeS",
-        Startups: {
-          data: [
-            {
-              id: 201,
-              attributes: {
-                Label: "Startup C",
-                order: 1,
-                Image: {
-                  data: {
-                    attributes: { url: "/img/Startups/Startup-c.png" }
-                  }
-                }
-              }
-            }
-          ]
-        }
-      }
-    }
-  ];
+     id: 4,
+     attributes: {
+       Type: "AVGC",
+       Startups: {
+         data: [
+           {
+             id: 16,
+             attributes: {
+               Label: "Startup C",
+               order: 1,
+               Image: {
+                 data: {
+                   attributes: { url: "/img/Startups/16s.png" }
+                 }
+               }
+             }
+           },
+           {
+             id: 17,
+             attributes: {
+               Label: "Startup C",
+               order: 1,
+               Image: {
+                 data: {
+                   attributes: { url: "/img/Startups/17s.png" }
+                 }
+               }
+             }
+           }
+         ]
+       }
+     }
+   },
+    {
+     id: 5,
+     attributes: {
+       Type: "Education",
+       Startups: {
+         data: [
+           {
+             id: 18,
+             attributes: {
+               Label: "Startup C",
+               order: 1,
+               Image: {
+                 data: {
+                   attributes: { url: "/img/Startups/18s.png" }
+                 }
+               }
+             }
+           },
+           {
+             id: 19,
+             attributes: {
+               Label: "Startup C",
+               order: 1,
+               Image: {
+                 data: {
+                   attributes: { url: "/img/Startups/19s.png" }
+                 }
+               }
+             }
+           },
+           {
+             id: 20,
+             attributes: {
+               Label: "Startup C",
+               order: 1,
+               Image: {
+                 data: {
+                   attributes: { url: "/img/Startups/20s.png" }
+                 }
+               }
+             }
+           }
+         ]
+       }
+     }
+   },
+    {
+     id: 6,
+     attributes: {
+       Type: "Consumer/emerging sectors",
+       Startups: {
+         data: [
+           {
+             id: 41,
+             attributes: {
+               Label: "Indimus Innovative LLP",
+               order: 1,
+               Image: {
+                 data: {
+                   attributes: { url: "/img/Startups/41s.png" }
+                 }
+               }
+             }
+           },
+           {
+             id: 42,
+             attributes: {
+               Label: "The Trudot Cosmetics",
+               order: 1,
+               Image: {
+                 data: {
+                   attributes: { url: "/img/Startups/42s.png" }
+                 }
+               }
+             }
+           },
+           {
+             id: 23,
+             attributes: {
+               Label: "Startup C",
+               order: 1,
+               Image: {
+                 data: {
+                   attributes: { url: "/img/startups/23.png" }
+                 }
+               }
+             }
+           }
+         ]
+       }
+     }
+   },
+    {
+     id: 7,
+     attributes: {
+       Type: "Electronics & Communication",
+       Startups: {
+         data: [
+           {
+             id: 24,
+             attributes: {
+               Label: "Indimus Innovative LLP",
+               order: 1,
+               Image: {
+                 data: {
+                   attributes: { url: "/img/Startups/24s.png" }
+                 }
+               }
+             }
+           },
+           {
+             id: 25,
+             attributes: {
+               Label: "The Trudot Cosmetics",
+               order: 1,
+               Image: {
+                 data: {
+                   attributes: { url: "/img/Startups/25s.png" }
+                 }
+               }
+             }
+           },
+         ]
+       }
+     }
+   }
+ ];
 
   // sort by Startupship type order if you have `attributes.order`
   data.sort((a, b) => (a.attributes.order || 0) - (b.attributes.order || 0));
@@ -74,16 +408,16 @@
 </script>
 
 <div class="px-8 items-center justify-center text-center mx-auto">
-  <div class="flex flex-wrap justify-center items-center gap-5">
+  <div class="flex flex-wrap justify-center items-center gap-8">
     {#each data as item (item.id)}
       {#if item.attributes.Startups && item.attributes.Startups.data.length > 0}
         <div
-          class={"flex flex-col flex-wrap text-center items-center justify-center " +
+          class={"flex flex-col flex-wrap text-center items-center justify-center  " +
             ((item.attributes.Startups.data.length > 7)
               ? "md:col-span-7"
               : "md:col-span-" + item.attributes.Startups.data.length)}
         >
-          <div class="text-center pb-2">
+          <div class="text-center pb-3 pt-2">
             {#if item.attributes.Type}
               <div class="text-base font-semibold text-black">
                 {item.attributes.Type}
@@ -92,11 +426,11 @@
           </div>
           <div class="flex flex-wrap flex-row gap-2 items-center justify-center">
             {#each item.attributes.Startups.data as Startup (Startup.id)}
-                <a class="rounded-md w-32 md:w-40 md:h-24">
+                <a class="rounded-md w-32 md:w-40 md:h-24 bg-white border">
                   <img
                     src={Startup.attributes.Image.data.attributes.url}
                     alt={Startup.attributes.Label}
-                    class="w-full h-full rounded-md mb-2"
+                    class="w-full h-full rounded-md mb-2 p-2"
                   />
                 </a>
             {/each}
