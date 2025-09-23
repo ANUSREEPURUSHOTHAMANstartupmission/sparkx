ce999d7601c7701265c166047781a1980124c4f7
Anusree Purushothaman <anusreepurushothaman@MacBook-Pro.local>
2025-09-23 14:32:59 +0530

    changes

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,12 +1,16 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {
-		extend: {},
-	},
-	plugins: [
-		require("tailwindcss-scoped-groups"),
-		require('flowbite/plugin'),
-		require('@tailwindcss/line-clamp'),
-	],
+  content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+  theme: {
+    screens: {
+      md: '45rem', // 720px
+      lg: '60rem', // 960px
+      xl: '75rem', // 1200px
+    },
+  },
+  plugins: [
+    require("tailwindcss-scoped-groups"),
+    require('flowbite/plugin'),
+    require('@tailwindcss/line-clamp'),
+  ],
 }
