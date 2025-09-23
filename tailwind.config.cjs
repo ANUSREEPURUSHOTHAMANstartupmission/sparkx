ee9507b06b254b611adc364ff5ca5f7cae05bfa3
Anusree Purushothaman <anusreepurushothaman@MacBook-Pro.local>
2025-09-23 14:03:42 +0530

    changes

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -3,16 +3,6 @@ module.exports = {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
 		extend: {},
-		 screens: {
-      'sm': '576px',
-      // => @media (min-width: 576px) { ... }
-
-      'md': '960px',
-      // => @media (min-width: 960px) { ... }
-
-      'lg': '1440px',
-      // => @media (min-width: 1440px) { ... }
-    },
 	},
 	plugins: [
 		require("tailwindcss-scoped-groups"),
